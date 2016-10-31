67063bcb103db05c62f49158cfebae0d8647bec9
miya0001 <miya@wpist.me>
2016-10-31 16:41:57 +0900

    setup travis ci

---
+++ bin/install-wp.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -ex;
+
+DB_USER=root
+DB_PASS=""
+DB_NAME=wordpress
+PORT=8080
+WP_PATH=/tmp/wordpress
+WP_TITLE='Welcome to the WordPress'
+WP_DESC='Hello World!'
+
+if [ -e "$WP_PATH/wp-config.php" ]; then
+    open http://127.0.0.1:$PORT
+    bin/wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
+    exit 0
+fi
+
+if ! bin/wp --info ; then
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar
+    rm -fr bin && mkdir bin
+    mv wp-cli-nightly.phar bin/wp
+    chmod 755 bin/wp
+fi
+
+echo "path: www" > $(pwd)/wp-cli.yml
+
+if [ $DB_PASS ]; then
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS
+else
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER
+fi
+
+bin/wp core download --path=$WP_PATH --locale=en_US --force
+
+if [ $DB_PASS ]; then
+bin/wp core config \
+--dbhost=localhost \
+--dbname="$DB_NAME" \
+--dbuser="$DB_USER" \
+--dbpass="$DB_PASS" \
+--dbprefix=wp_ \
+--locale=en_US \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+PHP
+else
+bin/wp core config \
+--dbhost=localhost \
+--dbname=$DB_NAME \
+--dbuser=$DB_USER \
+--dbprefix=wp_ \
+--locale=en_US \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+PHP
+fi
+
+bin/wp core install \
+--url=http://127.0.0.1:$PORT \
+--title="WordPress" \
+--admin_user="admin" \
+--admin_password="admin" \
+--admin_email="admin@example.com"
+
+bin/wp rewrite structure "/archives/%post_id%"
+
+bin/wp option update blogname "$WP_TITLE"
+bin/wp option update blogdescription "$WP_DESC"
+
+if [ -e "provision-post.sh" ]; then
+    bash provision-post.sh
+fi
+
+open http://127.0.0.1:$PORT
+bin/wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
