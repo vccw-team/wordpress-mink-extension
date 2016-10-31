ef5c52ce21c665005e36ca4d4f367cb8e939cec5
miya0001 <miya@wpist.me>
2016-10-31 17:04:53 +0900

    bug fix on wp installer

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -3,7 +3,6 @@
 set -ex;
 
 DB_USER=root
-DB_PASS=""
 DB_NAME=wordpress
 PORT=8080
 WP_PATH=/tmp/wordpress
@@ -25,30 +24,10 @@ fi
 
 echo "path: www" > $(pwd)/wp-cli.yml
 
-if [ $DB_PASS ]; then
-    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS
-    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS
-else
-    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER
-    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER
-fi
-
 bin/wp core download --path=$WP_PATH --locale=en_US --force
 
-if [ $DB_PASS ]; then
 bin/wp core config \
---dbhost=localhost \
---dbname="$DB_NAME" \
---dbuser="$DB_USER" \
---dbpass="$DB_PASS" \
---dbprefix=wp_ \
---locale=en_US \
---extra-php <<PHP
-define( 'JETPACK_DEV_DEBUG', true );
-define( 'WP_DEBUG', true );
-PHP
-else
-bin/wp core config \
+--path=$WP_PATH \
 --dbhost=localhost \
 --dbname=$DB_NAME \
 --dbuser=$DB_USER \
@@ -58,23 +37,23 @@ bin/wp core config \
 define( 'JETPACK_DEV_DEBUG', true );
 define( 'WP_DEBUG', true );
 PHP
-fi
 
 bin/wp core install \
+--path=$WP_PATH \
 --url=http://127.0.0.1:$PORT \
 --title="WordPress" \
 --admin_user="admin" \
 --admin_password="admin" \
 --admin_email="admin@example.com"
 
-bin/wp rewrite structure "/archives/%post_id%"
+bin/wp rewrite structure "/archives/%post_id%" --path=$WP_PATH \
 
-bin/wp option update blogname "$WP_TITLE"
-bin/wp option update blogdescription "$WP_DESC"
+bin/wp option update blogname "$WP_TITLE" --path=$WP_PATH \
+bin/wp option update blogdescription "$WP_DESC" --path=$WP_PATH \
 
 if [ -e "provision-post.sh" ]; then
     bash provision-post.sh
 fi
 
 open http://127.0.0.1:$PORT
-bin/wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
+bin/wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH --path=$WP_PATH
