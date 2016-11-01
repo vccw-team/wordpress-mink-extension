8f3e9f8b3be457cca7eb4dc3e00c10d7867ab33e
miya0001 <miya@wpist.me>
2016-11-01 23:14:06 +0900

    bug fix travis#29

--- bin/install-wp.sh
+++ bin/install-wp.sh
@@ -39,4 +39,4 @@ PHP
 ./wp-cli-nightly.phar option update blogname "$WP_TITLE" --path=$WP_PATH
 ./wp-cli-nightly.phar option update blogdescription "$WP_DESC" --path=$WP_PATH
 
-./wp-cli-nightly.phar user create editor editor@example.com --role=editor --user_pass=editor
+./wp-cli-nightly.phar user create editor editor@example.com --role=editor --user_pass=editor --path=$WP_PATH
