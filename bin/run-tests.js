0954473e3157b4fc753b2bd1fb3bf177a7811e5d
miya0001 <miya@wpist.me>
2016-11-06 13:23:16 +0900

    uninstall old nodejs

---
+++ bin/run-tests.js
@@ -0,0 +1,11 @@
+const phantomjs = require( 'phantomjs-prebuilt' )
+const spawn = require( 'child_process' ).spawn
+phantomjs.run(
+  '--webdriver=4444',
+  '--ignore-ssl-errors=yes',
+  '--cookies-file=/tmp/webdriver_cookie.txt'
+).then( program => {
+  const behat = spawn( 'vendor/bin/behat', [], { stdio: "inherit" } )
+  behat.stdout.pipe( process.stdout )
+  behat.on( 'exit', code => { program.kill() } )
+} )
