7d13393875307955e11fe9124f965acbe9dc4ff2
miya0001 <miya@wpist.me>
2016-12-24 03:28:05 +0900

    add php -d args

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -5,12 +5,19 @@ const argv = process.argv
 argv.shift()
 argv.shift()
 
+args = [
+  'php',
+  '-d',
+  'memory_limit=-1',
+  'vendor/bin/behat'
+]
+
 phantomjs.run(
   '--webdriver=4444',
   '--ignore-ssl-errors=yes',
   '--cookies-file=/tmp/webdriver_cookie.txt'
 ).then( program => {
-  const behat = spawn( 'vendor/bin/behat', argv, { stdio: "inherit" } )
+  const behat = spawn( '/usr/bin/env', args.concat( argv ), { stdio: "inherit" } )
   behat.on( 'exit', ( code ) => {
     program.kill()
     process.exit( code );
